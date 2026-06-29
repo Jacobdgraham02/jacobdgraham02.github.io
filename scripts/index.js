3ce8c0a2bd2c211fec349357799a895fb24162b8
Jacob Graham <jacobdgraham02@gmail.com>
2026-06-29 18:11:45 -0400

    Updated my picture to look less evil. Updated index.js script to remove unused code and add comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,46 @@
-const overlay = document.querySelector('.overlay');
-const menuBtn = document.querySelector('.menu_btn');
+// Variables selecting sections of the website
+const menu_btn = document.querySelector('.menu_btn');
 const menu = document.querySelector('.menu');
 const menu_links = document.querySelectorAll('.menu a');
 const website_section = document.querySelectorAll('.website_section');
 const current_section = "current_section_open";
 const current_link_visiting = "add_active_link_style";
-const project_divs = document.querySelectorAll('.project_div');
+
+// Strings to use with typeit element on screen to make message appear and disappear
 const home_page_sentences = 
 [
-    'Honours Computer Science graduate', 
+    'Software Developer', 
     'Communications Operator', 
-    'Software Developer',
     'Community Volunteer'
 ];
-menuBtn.addEventListener('change', function() {
-    if (menuBtn.checked) {
-        menu.style.left = '0';
-    } else {
-        menu.style.left = '-100%';
-    }
-});
+closeMenu();
+setSectionToActive();
+setDefaultLinkStyling();
+
+// Definition of TypeIt element used on application to define characteristics
 const typeit_element = new TypeIt("#text", {
     speed: 100,
     loop: true
 });
 
+// Iterates through the array home_page_sentences, inserting each one into the created TypeIt element
 for (const sentence_string of home_page_sentences) {
     typeit_element.type(sentence_string).pause(400).delete(sentence_string.length).pause(400);
 }
 typeit_element.go();
 
-closeMenu();
-setSectionToActive();
-setDefaultLinkStyling();
-
+// Defines dynamically changing styling by first definining the class styling definition in the CSS file, then applying that class to a given element
 function setDefaultLinkStyling(){
     menu_links[0].classList.add(current_link_visiting);
 }
-
+// Close the navigation menu box
 function closeMenu() {
     menu_links.forEach(link => 
         link.addEventListener("click", function() {
-            menuBtn.checked = false;
+            menu_btn.checked = false;
     }));
 }
+// Sets a section as active so it appears in the content section of the website
 function setSectionToActive() {
     menu_links.forEach(link => {
         link.addEventListener('click', function(event) {
@@ -59,23 +56,19 @@ function setSectionToActive() {
         });
     });
 }
+// Functionality for changing the main page section
 function addClassToSection(section, t) {
     section.classList.add(t);
 }
+// Functionality for changing the main page section
 function removeClassFromSection(section, t) {
     section.classList.remove(t);
 }
+// Functionality for changing the menu link styling
 function addStylingToActiveLink(link, t) {
     link.classList.add(t);
 }
+// Functionality for changing the menu link styling
 function removeStylingFromActiveLink(link, t) {
     link.classList.remove(t);
 }
-document.querySelectorAll('.timeline-item').forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelectorAll('.timeline-item').forEach(innerItem => {
-            innerItem.classList.remove('active');
-        });
-        item.classList.add('active');
-    });
-});
